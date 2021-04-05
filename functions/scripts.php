c86cb958af9fb424c8b2d1488f982c69905c07e3
Marcel Dillen <marcel@marceldillen.nl>
2021-04-05 14:11:06 +0200

    Update scripts.php

--- functions/scripts.php
+++ functions/scripts.php
@@ -4,7 +4,7 @@ if ( ! function_exists('dwp_add_admin_scripts')) {
   function dwp_add_admin_scripts() {  
     wp_register_script(
       'dwp_script', 
-      plugins_url('assets/js/dwp_scripts.js', __DIR__), 
+      plugins_url('assets/js/dwp_admin_page.js', __DIR__), 
       array('jquery'),
       '1.0', 
       true
